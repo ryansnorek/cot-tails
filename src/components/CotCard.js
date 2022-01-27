05266b4db73974f3920bbb892f8f0795b9a59e68
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-01-27 18:31:44 -0500

    wip

--- src/components/CotCard.js
+++ src/components/CotCard.js
@@ -22,49 +22,49 @@ export default function CotCard({ item }) {
             <h3>Deviation</h3>
           </section>
           <div className="report-data">
-          <section className={open_interest.filter ? "filter" : ""}>
+          <section className={open_interest.filter === false ? "filter" : ""}>
             <h4>Open Interest</h4>
             <p>{open_interest.current_report.toLocaleString()}</p>
             <p>{item.weekChange.open_interest.toLocaleString()}</p>
             <p>{open_interest.average.toLocaleString()}</p>
             <p>{open_interest.from_average.toLocaleString()}</p>
           </section>
-          <section className={asset_mgr_long.filter ? "filter" : ""}>
+          <section className={asset_mgr_long.filter === false ? "filter" : ""}>
             <h4>Asset Managers Long</h4>
             <p>{asset_mgr_long.current_report.toLocaleString()}</p>
             <p>{item.weekChange.asset_mgr_long.toLocaleString()}</p>
             <p>{asset_mgr_long.average.toLocaleString()}</p>
             <p>{asset_mgr_long.from_average.toLocaleString()}</p>
           </section>
-          <section className={asset_mgr_short.filter ? "filter" : ""}>
+          <section className={asset_mgr_short.filter === false ? "filter" : ""}>
             <h4>Asset Managers Short</h4>
             <p>{asset_mgr_short.current_report.toLocaleString()}</p>
             <p>{item.weekChange.asset_mgr_short.toLocaleString()}</p>
             <p>{asset_mgr_short.average.toLocaleString()}</p>
             <p>{asset_mgr_short.from_average.toLocaleString()}</p>
           </section>
-          <section className={lev_money_long.filter ? "filter" : ""}>
+          <section className={lev_money_long.filter === false ? "filter" : ""}>
             <h4>Leveraged Money Long</h4>
             <p>{lev_money_long.current_report.toLocaleString()}</p>
             <p>{item.weekChange.lev_money_long.toLocaleString()}</p>
             <p>{lev_money_long.average.toLocaleString()}</p>
             <p>{lev_money_long.from_average.toLocaleString()}</p>
           </section>
-          <section className={lev_money_short.filter ? "filter" : ""}>
+          <section className={lev_money_short.filter === false ? "filter" : ""}>
             <h4>Leveraged Money Short</h4>
             <p>{lev_money_short.current_report.toLocaleString()}</p>
             <p>{item.weekChange.lev_money_short.toLocaleString()}</p>
             <p>{lev_money_short.average.toLocaleString()}</p>
             <p>{lev_money_short.from_average.toLocaleString()}</p>
           </section>
-          <section className={nonrept_positions_long.filter ? "filter" : ""}>
+          <section className={nonrept_positions_long.filter === false ? "filter" : ""}>
             <h4>Non Reportables Long</h4>
             <p>{nonrept_positions_long.current_report.toLocaleString()}</p>
             <p>{item.weekChange.nonrept_positions_long.toLocaleString()}</p>
             <p>{nonrept_positions_long.average.toLocaleString()}</p>
             <p>{nonrept_positions_long.from_average.toLocaleString()}</p>
           </section>
-          <section className={nonrept_positions_short.filter ? "filter" : ""}>
+          <section className={nonrept_positions_short.filter === false ? "filter" : ""}>
             <h4>Non Reportables Short</h4>
             <p>{nonrept_positions_short.current_report.toLocaleString()}</p>
             <p>{item.weekChange.nonrept_positions_short.toLocaleString()}</p>
