0b4c8513e99449a57d10ee9c56bd13a00507626c
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-01-27 13:47:59 -0500

    wip

--- src/hooks/useReportData.js
+++ src/hooks/useReportData.js
@@ -7,13 +7,10 @@ import analyzeReport from "../helper/analyzeReport";
 export default function useReportData(year) {
   const [currentCotReport, setCurrentCotReport] = useState([]);
   const [historyMetrics, setHistoryMetrics] = useState([]);
-  const [loadingMetrics, setLoadingMetrics] = useState(false);
-  const [loadingReport, setLoadingReport] = useState(false);
   const [analyzedReport, setAnalyzedReport] = useState([]);
 
   useEffect(function getCotReport() {
     if (currentCotReport.length === 0) {
-      setLoadingReport(true);
       axios
         .get(`${BACKEND_URL}/api/cot/report`)
         .then((report) => {
@@ -23,38 +20,37 @@ export default function useReportData(year) {
           setCurrentCotReport(cleanReport);
         })
         .catch((err) => {
-          console.log("ERROR =-=--=-=", err);
+          console.error("Error fetching report =-=--=-=", err);
         });
     }
-    return () => setLoadingReport(false);
   }, []);
 
   useEffect(function getMetrics() {
     if (historyMetrics.length === 0) {
-      setLoadingMetrics(true);
       axios
         .get(`${BACKEND_URL}/api/cot/history/metrics/${year}`)
         .then((metrics) => {
           setHistoryMetrics(metrics.data);
         })
         .catch((err) => {
-          console.log("ERROR =-=--=-=", err);
+          console.error("Error fetching history metrics =-=--=-=", err);
         });
     }
-    return () => setLoadingMetrics(false);
   }, []);
 
   useEffect(
     function reportAnalysis() {
-      if (currentCotReport.length > 1 && historyMetrics.length > 1 && analyzedReport.length === 0) {
+      if (
+        currentCotReport.length > 1 &&
+        historyMetrics.length > 1 &&
+        analyzedReport.length === 0
+      ) {
         const report = analyzeReport(currentCotReport, historyMetrics);
         setAnalyzedReport(report);
       }
-      return () => setLoadingReport(false)
     },
     [historyMetrics, currentCotReport]
   );
 
-
-  return [analyzedReport, loadingMetrics, loadingReport];
+  return [analyzedReport];
 }
