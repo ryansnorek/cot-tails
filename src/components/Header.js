31f9712854a435b5ff6dc31d7445d292900babf3
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-02-05 12:54:25 -0500

    wip

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,58 @@
 export default function Header({ formValues, handleChange }) {
   return (
     <header className="top">
-      <img className="logo" src="../../logo.png" alt="logo" />
-      <h1>Commitment of Traders</h1>
-      <div className="search-bar">
-        <label htmlFor="search">Search:</label>
-        <input
-          type="search"
-          name="search"
-          value={formValues.search}
-          onChange={handleChange}
-        />
+      <div className="title">
+        <img className="logo" src="../../logo.png" alt="logo" />
+        <h1>Commitment of Traders</h1>
       </div>
-      <div className="user-select">
-        <label>
-          Comparison year
-          <select
-            name="year"
-            className="dropdown"
-            value={formValues.year}
+      <div className="filters">
+        <div className="search-bar">
+          <input
+            type="search"
+            name="search"
+            placeholder="Search assets"
+            value={formValues.search}
             onChange={handleChange}
-          >
-            <option value={2021}>2021</option>
-            <option value={2020}>2020</option>
-            <option value={2019}>2019</option>
-            <option value={2018}>2018</option>
-            <option value={2017}>2017</option>
-            <option value={2016}>2016</option>
-            <option value={2015}>2015</option>
-            <option value={2014}>2014</option>
-          </select>
-        </label>
-        <label>
-          Deviation filter
-          <select
-            name="deviation"
-            className="dropdown"
-            value={formValues.deviation}
-            onChange={handleChange}
-          >
-            <option value={0}>None</option>
-            <option value={200}>200%</option>
-            <option value={150}>150%</option>
-            <option value={125}>125%</option>
-            <option value={100}>100%</option>
-            <option value={75}>75%</option>
-            <option value={50}>50%</option>
-            <option value={25}>25%</option>
-          </select>
-        </label>
+          />
+        </div>
+        <div className="user-select">
+          <label>
+            Comparison year
+            <select
+              name="year"
+              className="dropdown"
+              value={formValues.year}
+              onChange={handleChange}
+            >
+              <option value={2021}>2021</option>
+              <option value={2020}>2020</option>
+              <option value={2019}>2019</option>
+              <option value={2018}>2018</option>
+              <option value={2017}>2017</option>
+              <option value={2016}>2016</option>
+              <option value={2015}>2015</option>
+              <option value={2014}>2014</option>
+            </select>
+          </label>
+          <label>
+            Deviation filter
+            <select
+              name="deviation"
+              className="dropdown"
+              value={formValues.deviation}
+              onChange={handleChange}
+            >
+              <option value={0}>None</option>
+              <option value={200}>200%</option>
+              <option value={150}>150%</option>
+              <option value={125}>125%</option>
+              <option value={100}>100%</option>
+              <option value={75}>75%</option>
+              <option value={50}>50%</option>
+              <option value={25}>25%</option>
+            </select>
+          </label>
+        </div>
       </div>
     </header>
   );
