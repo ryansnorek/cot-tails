8013befffb376294777d5a7cbc1fc30fb65a6ba1
Ryan Snorek <ryan.snorek@manamonitoring.com>
2022-02-19 16:21:24 -0500

    wip

--- src/components/CotReport.js
+++ src/components/CotReport.js
@@ -21,7 +21,7 @@ function CotReport({ formValues, setReportDate }) {
             <CotCard key={item.title} item={item} formValues={formValues} />
           );
         })}
-        || <Skeleton />
+        || <Skeleton count={100} />
       </section>
     </div>
   );
